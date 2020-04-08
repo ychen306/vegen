12876ce1c679c33e742713b927234385338bad09
Yishen Chen <ychen306@mit.edu>
2020-04-07 20:45:59 -0400

    move impl of packer into packer.cpp

--- gslp/GSLP.cpp
+++ gslp/GSLP.cpp
@@ -6,6 +6,7 @@
 #include "VectorPack.h"
 #include "VectorPackContext.h"
 #include "VectorPackSet.h"
+#include "Packer.h"
 #include "llvm/ADT/BitVector.h"
 #include "llvm/ADT/PostOrderIterator.h"
 #include "llvm/ADT/SmallPtrSet.h"
@@ -200,8 +201,6 @@ struct MCMCVectorPackSet : public VectorPackSet {
   const VectorPack *removeRandomPack();
 };
 
-bool isScalarType(Type *Ty) { return Ty->getScalarType() == Ty; }
-
 bool hasFeature(const llvm::Function &F, std::string Feature) {
   Attribute Features = F.getFnAttribute("target-features");
   return !Features.hasAttribute(Attribute::None) &&
@@ -216,24 +215,6 @@ bool isSupported(InstBinding *Inst, const llvm::Function &F) {
 }
 
 } // end anonymous namespace
-
-// Do a quadratic search to build the access dags
-template <typename MemAccessTy>
-void buildAccessDAG(ConsecutiveAccessDAG &DAG, ArrayRef<MemAccessTy *> Accesses,
-                    const DataLayout *DL, ScalarEvolution *SE) {
-  for (auto *A1 : Accesses) {
-    // Get type of the value being acccessed
-    auto *Ty =
-        cast<PointerType>(A1->getPointerOperand()->getType())->getElementType();
-    if (!isScalarType(Ty))
-      continue;
-    for (auto *A2 : Accesses) {
-      if (A1->getType() == A2->getType() &&
-          isConsecutiveAccess(A1, A2, *DL, *SE))
-        DAG[A1].insert(A2);
-    }
-  }
-};
 
 IRVectorBinding IRVectorBinding::Create(const BinaryIROperation *Op,
                                         unsigned VectorWidth) {
@@ -267,9 +248,6 @@ Value *IRVectorBinding::emit(llvm::ArrayRef<llvm::Value *> Operands,
 }
 
 char GSLP::ID = 0;
-
-// Sample an integer between 0 and N.
-static unsigned rand_int(int N) { return std::rand() % N; }
 
 // Sample a float between 0 and 1
 float rand_float() {
@@ -473,288 +451,6 @@ const VectorPack *MCMCVectorPackSet::removeRandomPack() {
   NumPacks--;
   return VP;
 }
-
-template <typename InstTy>
-Optional<SmallVector<InstTy *, 4>>
-castOperandPack(const VectorPack::OperandPack &OpndPack) {
-  SmallVector<InstTy *, 4> Ret;
-  for (auto *V : OpndPack) {
-    auto *I = dyn_cast<InstTy>(V);
-    if (!I)
-      return None;
-    Ret.push_back(I);
-  }
-  return Ret;
-}
-
-// FIXME: remove Insts and take the list of all supported instruction instead
-// FIXME: ignore lane order here.
-// Find vector packs that produces operand pack
-static void extendWithDef(
-    const VectorPack::OperandPack &OpndPack, const VectorPackSet &ExistingPacks,
-    std::vector<VectorPack *> &Extensions,
-    DenseMap<BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>> &LoadDAGs,
-    DenseMap<BasicBlock *, std::unique_ptr<MatchManager>> &MMs,
-    DenseMap<BasicBlock *, std::unique_ptr<VectorPackContext>> &VPCtxs,
-    DenseMap<BasicBlock *, std::unique_ptr<LocalDependenceAnalysis>> &LDAs,
-    DenseMap<BasicBlock *, SmallPtrSet<const InstBinding *, 4>> &Insts,
-    TargetTransformInfo *TTI) {
-  BitVector Elements;
-  BitVector Depended;
-
-  BasicBlock *BB = nullptr;
-
-  VectorPackContext *VPCtx = nullptr;
-  MatchManager *MM = nullptr;
-  ConsecutiveAccessDAG *LoadDAG = nullptr;
-
-  // First, check if the operand pack is indepdendent.
-  // We can't extend if it's not independent.
-  for (Value *V : OpndPack) {
-    // Also bail if we can't produce this pack at current basic block
-    auto *I = dyn_cast<Instruction>(V);
-    if (!I)
-      return;
-    if (!BB) {
-      BB = I->getParent();
-
-      VPCtx = VPCtxs[BB].get();
-      MM = MMs[BB].get();
-      LoadDAG = LoadDAGs[BB].get();
-
-      Elements = BitVector(VPCtx->getNumValues());
-      Depended = BitVector(VPCtx->getNumValues());
-    } else if (I->getParent() != BB)
-      return;
-
-    assert(VPCtx);
-
-    // Check dependence
-    unsigned ValueId = VPCtx->getScalarId(I);
-    if (Elements.test(ValueId))
-      return;
-
-    BitVector Depended2 = LDAs[BB]->getDepended(I);
-    if (Depended.test(ValueId))
-      return;
-    if (Depended2.anyCommon(Elements))
-      return;
-
-    Elements.set(ValueId);
-    Depended |= Depended2;
-  }
-
-  if (auto LoadsOrNull = castOperandPack<LoadInst>(OpndPack)) {
-    std::vector<LoadInst *> Loads;
-    SmallPtrSet<LoadInst *, 4> LoadSet(LoadsOrNull->begin(),
-                                       LoadsOrNull->end());
-    for (auto *LI : LoadsOrNull.getValue()) {
-      SmallPtrSet<LoadInst *, 4> LoadsRemained = LoadSet;
-      // See if there's a proper ordering of these loads starting from `LI`,
-      // so that they are consecutive.
-      LoadsRemained.erase(LI);
-      LoadInst *CurLoad = LI;
-      Loads = {LI};
-      while (!LoadsRemained.empty()) {
-        auto It = LoadDAG->find(CurLoad);
-        if (It == LoadDAG->end())
-          break;
-        LoadInst *NextLoad = nullptr;
-        for (auto *I : It->second) {
-          auto *LI = cast<LoadInst>(I);
-          if (LoadsRemained.erase(LI)) {
-            NextLoad = LI;
-            break;
-          }
-        }
-        if (!NextLoad)
-          break;
-        Loads.push_back(NextLoad);
-        CurLoad = NextLoad;
-      }
-      if (Loads.size() == LoadSet.size())
-        break;
-    }
-    if (Loads.size() != LoadSet.size())
-      return;
-    Extensions.push_back(VPCtx->createLoadPack(Loads, Elements, Depended, TTI));
-    return;
-  }
-
-  if (auto PHIsOrNull = castOperandPack<PHINode>(OpndPack)) {
-    Extensions.push_back(VPCtx->createPhiPack(PHIsOrNull.getValue(), TTI));
-    return;
-  }
-
-  // NOTE: We can't extend with Store packs vector stores don't produce
-  // anything...
-
-  // Aux func to enumerate cross product of `LaneMatches`
-  auto EnumeratePacks =
-      [&](const InstBinding *Inst,
-          const std::vector<ArrayRef<Operation::Match>> &LaneMatches) {
-        assert(Inst->getLaneOps().size() == LaneMatches.size());
-        unsigned N = 1;
-        for (auto &Matches : LaneMatches)
-          N *= Matches.size();
-        for (unsigned i = 0; i < N; i++) {
-          // `i` represent a particular member of the cross product.
-          // Decode `i` here.
-          unsigned Encoded = i;
-          std::vector<const Operation::Match *> Lanes;
-          for (auto &Matches : LaneMatches) {
-            unsigned M = Matches.size();
-            Lanes.push_back(&Matches[Encoded % M]);
-            Encoded /= M;
-          }
-
-          Extensions.push_back(
-              VPCtx->createVectorPack(Lanes, Elements, Depended, Inst, TTI));
-        }
-      };
-  std::vector<ArrayRef<Operation::Match>> LaneMatches;
-  for (auto *Inst : Insts[BB]) {
-    // See if we can pack with this Inst
-    ArrayRef<BoundOperation> LaneOps = Inst->getLaneOps();
-    unsigned NumLanes = LaneOps.size();
-    if (NumLanes != OpndPack.size())
-      continue;
-    LaneMatches.resize(NumLanes);
-    bool Feasible = true;
-    unsigned LaneId = 0;
-    for (const auto &LaneOp : LaneOps) {
-      ArrayRef<Operation::Match> Matches =
-          MM->getMatchesForOutput(LaneOp.getOperation(), OpndPack[LaneId]);
-      if (Matches.empty()) {
-        Feasible = false;
-        break;
-      }
-      LaneMatches[LaneId] = Matches;
-      LaneId++;
-    }
-    if (Feasible)
-      EnumeratePacks(Inst, LaneMatches);
-  }
-}
-
-class Packer {
-  // FIXME: fuse all of these together into a single map
-  DenseMap<BasicBlock *, std::unique_ptr<MatchManager>> MMs;
-  DenseMap<BasicBlock *, std::unique_ptr<LocalDependenceAnalysis>> LDAs;
-  DenseMap<BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>> LoadDAGs;
-  DenseMap<BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>> StoreDAGs;
-  DenseMap<BasicBlock *, std::unique_ptr<VectorPackContext>> VPCtxs;
-
-  std::vector<InstBinding *> SupportedInsts;
-
-  TargetTransformInfo *TTI;
-  BlockFrequencyInfo *BFI;
-
-  // First find out if which vector instruction we can emit.
-  // E.g. sometimes there is simply no `fadd` in a basic block..
-  DenseMap<BasicBlock *, SmallPtrSet<const InstBinding *, 4>> InstBindings;
-
-public:
-  Packer(ArrayRef<InstBinding *> SupportedInsts, llvm::Function &F,
-         AliasAnalysis *AA, const DataLayout *DL, ScalarEvolution *SE,
-         TargetTransformInfo *TTI, BlockFrequencyInfo *BFI)
-      : SupportedInsts(SupportedInsts.vec()), TTI(TTI), BFI(BFI) {
-    // Setup analyses and determine search space
-    for (auto &BB : F) {
-      std::vector<LoadInst *> Loads;
-      std::vector<StoreInst *> Stores;
-      // Find packable instructions
-      auto MM = std::make_unique<MatchManager>(SupportedInsts, BB);
-      for (auto &I : BB) {
-        if (auto *LI = dyn_cast<LoadInst>(&I)) {
-          if (LI->isSimple())
-            Loads.push_back(LI);
-        } else if (auto *SI = dyn_cast<StoreInst>(&I)) {
-          if (SI->isSimple())
-            Stores.push_back(SI);
-        }
-      }
-
-      auto VPCtx = std::make_unique<VectorPackContext>(&BB);
-      auto LoadDAG = std::make_unique<ConsecutiveAccessDAG>();
-      auto StoreDAG = std::make_unique<ConsecutiveAccessDAG>();
-      buildAccessDAG<LoadInst>(*LoadDAG, Loads, DL, SE);
-      buildAccessDAG<StoreInst>(*StoreDAG, Stores, DL, SE);
-
-      MMs[&BB] = std::move(MM);
-      LDAs[&BB] =
-          std::make_unique<LocalDependenceAnalysis>(AA, &BB, VPCtx.get());
-      VPCtxs[&BB] = std::move(VPCtx);
-      LoadDAGs[&BB] = std::move(LoadDAG);
-      StoreDAGs[&BB] = std::move(StoreDAG);
-    }
-
-    VectorPackSet TempPacks(&F);
-    for (auto *Inst : SupportedInsts) {
-      for (auto &BB : F) {
-        auto VPOrNull = sampleVectorPack(TempPacks, *MMs[&BB], *VPCtxs[&BB],
-                                         *LDAs[&BB], Inst, TTI, 1000);
-        if (VPOrNull)
-          InstBindings[&BB].insert(Inst);
-      }
-    }
-  }
-
-  void findExtensionForOnePack(const VectorPack &VP, const VectorPackSet &Packs,
-                               std::vector<VectorPack *> &Extensions) {
-    for (auto &OpndPack : VP.getOperandPacks()) {
-      // Figure out where the scalar operands are produced.
-      // Bail if they are produced in different basic blocks.
-      BasicBlock *BB = nullptr;
-      bool FromSingleBB = true;
-      for (auto *V : OpndPack) {
-        auto *I = dyn_cast<Instruction>(V);
-        if (!I)
-          continue;
-        if (!BB)
-          BB = I->getParent();
-        else if (I->getParent() != BB) {
-          FromSingleBB = false;
-          break;
-        }
-      }
-      // Can't extend from this operand pack
-      if (!FromSingleBB || !BB)
-        break;
-
-      extendWithDef(OpndPack, Packs, Extensions, LoadDAGs, MMs, VPCtxs, LDAs,
-                    InstBindings, TTI);
-    }
-  }
-
-  float evalSeedPacks(const VectorPackSet &Packs, unsigned Alpha = 4) {
-    unsigned NumTrials = Alpha * Packs.getNumPacks();
-    float BestCost = Packs.getCostSaving(TTI, BFI);
-    std::vector<VectorPack *> Extensions;
-    for (unsigned i = 0; i < NumTrials; i++) {
-      VectorPackSet ScratchPacks = Packs;
-      bool Changed;
-      unsigned FirstUnprocessedPackId = 0;
-      do {
-        Changed = false;
-        Extensions.clear();
-        for (unsigned i = FirstUnprocessedPackId;
-             i < ScratchPacks.getNumPacks(); i++)
-          findExtensionForOnePack(ScratchPacks.getPack(i), ScratchPacks,
-                                  Extensions);
-        FirstUnprocessedPackId = ScratchPacks.getNumPacks() - 1;
-        random_shuffle(Extensions.begin(), Extensions.end(), rand_int);
-        for (auto *VP : Extensions)
-          Changed |= ScratchPacks.tryAdd(VP);
-      } while (Changed);
-      float Cost = ScratchPacks.getCostSaving(TTI, BFI);
-      if (Cost < BestCost) {
-        BestCost = Cost;
-      }
-    }
-    return BestCost;
-  };
-};
 
 bool GSLP::runOnFunction(llvm::Function &F) {
   // if (F.getName() != "adi")
@@ -1017,8 +713,7 @@ bool GSLP::runOnFunction(llvm::Function &F) {
       //      Packs.tryAdd(PS.VP);
       //  }
       //}
-      PackSample PS = PackDistr.sample(Index, I, Packs, SupportedInsts, LDAs,
-                                       LoadDAGs, StoreDAGs, VPCtxs, MMs, TTI);
+      PackSample PS = Packer.samplePackForInst(I, Packs, PackDistr);
       LogProb += PS.LogProb;
       if (PS.VP)
         Packs.tryAdd(PS.VP);
---
+++ gslp/Packer.h
@@ -0,0 +1,97 @@
+#ifndef PACKER_H
+#define PACKER_H
+
+#include "IRModel.h"
+#include "InstSema.h"
+#include "LocalDependenceAnalysis.h"
+#include "MatchManager.h"
+#include "Util.h"
+#include "VectorPackContext.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/IR/Function.h"
+
+namespace llvm {
+class ScalarEvolution;
+class TargetTransformInfo;
+} // namespace llvm
+
+// Do a quadratic search to build the access dags
+template <typename MemAccessTy>
+void buildAccessDAG(ConsecutiveAccessDAG &DAG,
+                    llvm::ArrayRef<MemAccessTy *> Accesses,
+                    const llvm::DataLayout *DL, llvm::ScalarEvolution *SE) {
+  using namespace llvm;
+  for (auto *A1 : Accesses) {
+    // Get type of the value being acccessed
+    auto *Ty =
+        cast<PointerType>(A1->getPointerOperand()->getType())->getElementType();
+    if (!isScalarType(Ty))
+      continue;
+    for (auto *A2 : Accesses) {
+      if (A1->getType() == A2->getType() &&
+          isConsecutiveAccess(A1, A2, *DL, *SE))
+        DAG[A1].insert(A2);
+    }
+  }
+};
+
+// FIXME: remove Insts and take the list of all supported instruction instead
+// FIXME: ignore lane order here.
+// Find vector packs that produces operand pack
+void extendWithDef(
+    const VectorPack::OperandPack &OpndPack, const VectorPackSet &ExistingPacks,
+    std::vector<VectorPack *> &Extensions,
+    llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>>
+        &LoadDAGs,
+    llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<MatchManager>> &MMs,
+    llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<VectorPackContext>>
+        &VPCtxs,
+    llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<LocalDependenceAnalysis>>
+        &LDAs,
+    llvm::DenseMap<llvm::BasicBlock *,
+                   llvm::SmallPtrSet<const InstBinding *, 4>> &Insts,
+    llvm::TargetTransformInfo *TTI);
+
+class Packer {
+  // FIXME: fuse all of these together into a single map
+  llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<MatchManager>> MMs;
+  llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<LocalDependenceAnalysis>>
+      LDAs;
+  llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>>
+      LoadDAGs;
+  llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<ConsecutiveAccessDAG>>
+      StoreDAGs;
+  llvm::DenseMap<llvm::BasicBlock *, std::unique_ptr<VectorPackContext>> VPCtxs;
+
+  std::vector<InstBinding *> SupportedInsts;
+
+  llvm::TargetTransformInfo *TTI;
+  llvm::BlockFrequencyInfo *BFI;
+  IRIndex Index;
+
+  // First find out if which vector instruction we can emit.
+  // E.g. sometimes there is simply no `fadd` in a basic block..
+  llvm::DenseMap<llvm::BasicBlock *, llvm::SmallPtrSet<const InstBinding *, 4>>
+      InstBindings;
+
+public:
+  Packer(llvm::ArrayRef<InstBinding *> SupportedInsts, llvm::Function &F,
+         llvm::AliasAnalysis *AA, const llvm::DataLayout *DL,
+         llvm::ScalarEvolution *SE, llvm::TargetTransformInfo *TTI,
+         llvm::BlockFrequencyInfo *BFI);
+
+  void findExtensionForOnePack(const VectorPack &VP, const VectorPackSet &Packs,
+                               std::vector<VectorPack *> &Extensions);
+
+  float evalSeedPacks(const VectorPackSet &Packs, unsigned Alpha = 4);
+
+  PackSample samplePackForInst(llvm::Instruction *I,
+                               VectorPackSet &ExistingPacks,
+                               PackDistribution &PackDistr) {
+    return PackDistr.sample(Index, I, ExistingPacks, SupportedInsts, LDAs,
+                            LoadDAGs, StoreDAGs, VPCtxs, MMs, TTI);
+  }
+};
+
+#endif // PACKER
