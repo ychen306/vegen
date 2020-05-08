557c88cb17b93b119fea99e9fc38c0d71c474fb7
Yishen Chen <ychen306@mit.edu>
2020-05-08 10:52:10 -0400

    format

--- gslp/Serialize.h
+++ gslp/Serialize.h
@@ -4,7 +4,6 @@
 #include "Proto/serialize.pb.h"
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
-
 /*
  * This file provides the utility to serialize a target policy,
  * which consists of a frontier and the pack distribution
@@ -25,9 +24,7 @@ struct ProcessedFrontier {
 };
 
 struct ProcessedVectorPack {
-  enum Kind {
-    General, Store, Load
-  };
+  enum Kind { General, Store, Load };
 
   Kind K;
   unsigned InstId;
@@ -46,16 +43,17 @@ struct PolicySupervision {
 
 class PolicyReader {
   google::protobuf::io::FileInputStream IS;
+
 public:
   PolicyReader(int FD) : IS(FD) { IS.SetCloseOnDelete(true); }
-  void read(std::vector<ProcessedFrontier> &,
-      std::vector<ProcessedVectorPack> &);
+  void read(PolicySupervision &);
 };
 
 class PolicyWriter {
   google::protobuf::io::FileOutputStream OS;
+
 public:
-  PolicyWriter(int FD) : OS(FD) {OS.SetCloseOnDelete(true);}
-  void writeOne(const ProcessedFrontier &, const ProcessedVectorPack &);
+  PolicyWriter(int FD) : OS(FD) { OS.SetCloseOnDelete(true); }
+  void writeOne(const PolicySupervision &);
 };
 #endif // SERIALIZE_H
