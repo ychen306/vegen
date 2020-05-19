1d50a23e0ae706365ad873e5fa6cde3a8581ecbf
Yishen Chen <ychen306@mit.edu>
2020-05-19 14:01:02 -0400

    some more debug

--- gslp/train.cpp
+++ gslp/train.cpp
@@ -235,6 +235,9 @@ int main(int argc, char **argv) {
         //                     {(int64_t)Supervision[i].Prob.size()},
         //                     torch::TensorOptions().dtype(torch::kFloat32));
         //Targets.push_back(Target);
+        std::cerr <<    torch::from_blob(const_cast<float *>(Supervision[i].Prob.data()),
+                             {(int64_t)Supervision[i].Prob.size()},
+                             torch::TensorOptions().dtype(torch::kFloat32)) << '\n';
         auto &Prob = Supervision[i].Prob;
         auto It = std::max_element(Prob.begin(), Prob.end());
         Losses.push_back(-Probs[i][It-Prob.begin()].log());
