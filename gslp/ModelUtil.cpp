d86b4b758009db2c937c06d459909c1e0727a9b4
Yishen Chen <ychen306@mit.edu>
2020-05-19 19:19:05 -0400

    clamp min prob

--- gslp/ModelUtil.cpp
+++ gslp/ModelUtil.cpp
@@ -66,7 +66,7 @@ std::vector<torch::Tensor> BatchPackProbability::get() {
   unsigned Offset = 0;
   for (unsigned N : NumPacks) {
     auto Prob = RawProb.slice(0 /*dim*/, Offset, Offset + N);
-    Probs.push_back(Prob / Prob.sum());
+    Probs.push_back((Prob / Prob.sum()).clamp_min(1e-8));
     Offset += N;
   }
   return Probs;
