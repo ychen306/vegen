ae7bbcccc381cb3c5ab6581f8a2ce00f45305d15
tom <ychen306@mit.edu>
2021-05-14 13:53:47 -0400

    rm

--- flags-mcts.sh
+++
@@ -1,2 +0,0 @@
-VEGEN_BUILD_DIR=$1
-export CLANG_FLAGS="-O3 -Xclang -load -Xclang ${VEGEN_BUILD_DIR}/gslp/libGSLP.so -fno-slp-vectorize -mllvm --wrappers-dir=${VEGEN_BUILD_DIR} -march=haswell -mavx2 -ffast-math"
