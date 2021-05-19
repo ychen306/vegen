6d86567d48fd31cd73348716bbd0f17e3861f5a7
tom <ychen306@mit.edu>
2021-05-18 20:37:56 -0400

    rm

--- gslp/MatchManager.cpp
+++ gslp/MatchManager.cpp
@@ -4,43 +4,6 @@
 
 using namespace llvm;
 
-// E.g., the intermediate instruction(s) of `(a + b) + c` is `a+b`.
-void getIntermediateInsts(const Operation::Match &M,
-                          SmallPtrSetImpl<Instruction *> &Intermediates) {
-  SmallPtrSet<Value *, 4> LiveIns;
-  for (auto *V : M.Inputs)
-    LiveIns.insert(V);
-
-  std::function<void(Instruction *)> CollectIntermediates =
-      [&](Instruction *I) {
-        if (LiveIns.count(I))
-          return;
-        Intermediates.insert(I);
-        for (Value *O : I->operands())
-          if (auto *OI = dyn_cast<Instruction>(O))
-            CollectIntermediates(OI);
-      };
-
-  CollectIntermediates(cast<Instruction>(M.Output));
-}
-
-static bool hasExternalUses(const Operation::Match &M) {
-  SmallPtrSet<Instruction *, 2> Intermediates;
-  getIntermediateInsts(M, Intermediates);
-  for (auto *I : Intermediates) {
-    if (I == M.Output)
-      continue;
-    for (User *U : I->users()) {
-      auto *UI = dyn_cast<Instruction>(U);
-      if (UI && UI != M.Output && !Intermediates.count(UI)) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
 bool MatchManager::sortByOutput(const Operation::Match &A,
                                 const Operation::Match &B) {
   return A.Output < B.Output;
@@ -63,16 +26,6 @@ MatchManager::MatchManager(llvm::ArrayRef<InstBinding *> Insts,
 
   for (auto &KV : OpMatches) {
     auto &Matches = KV.second;
-
-#if 0
-    // Filter out matches with intermediates that have external uses
-    std::vector<unsigned> ToRemove;
-    for (unsigned i = 0; i < Matches.size(); i++)
-      if (hasExternalUses(Matches[i]))
-        ToRemove.push_back(i);
-    remove(Matches, ToRemove);
-    errs() << " REMOVING " << ToRemove.size() << '\n';
-#endif
 
     std::stable_sort(Matches.begin(), Matches.end(), sortByOutput);
   }
