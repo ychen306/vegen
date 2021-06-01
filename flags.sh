98d426757ce94424047fc8042d1a5f538900e528
tom <ychen306@mit.edu>
2021-06-01 13:55:50 -0400

    tweak

--- flags.sh
+++ flags.sh
@@ -1,2 +1,2 @@
 VEGEN_BUILD_DIR=$1
-export CLANG_FLAGS="-Xclang -load -Xclang ${VEGEN_BUILD_DIR}/gslp/libGSLP.so -fno-slp-vectorize -mllvm --wrappers-dir=${VEGEN_BUILD_DIR}"
+export CLANG_FLAGS="-Xclang -load -Xclang ${VEGEN_BUILD_DIR}/gslp/libGSLP.so -O3 -fno-slp-vectorize -mllvm --wrappers-dir=${VEGEN_BUILD_DIR}"
