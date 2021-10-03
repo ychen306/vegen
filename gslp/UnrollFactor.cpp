517cc090bdec89b254370cf652752e82de19a3f8
tom <ychen306@mit.edu>
2021-10-03 12:42:49 -0400

    k

--- gslp/UnrollFactor.cpp
+++ gslp/UnrollFactor.cpp
@@ -364,6 +364,9 @@ static void refineUnrollFactors(Function *F, DominatorTree &DT, LoopInfo &LI,
     UFs[L] = PowerOf2Ceil(UFs[L]) * divideCeil(4, NumRdxs);
     errs() << "Adjusted uf = " << UFs[L] << '\n';
   }
+
+  for (auto &Pair : UFs)
+    Pair.second = PowerOf2Ceil(Pair.second);
 }
 
 void computeUnrollFactorImpl(ArrayRef<const InstBinding *> Insts,
