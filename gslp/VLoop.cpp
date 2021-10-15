34dca5d45fccf1d983d3e6ffcf64fa778c93e962
tom <ychen306@mit.edu>
2021-10-15 14:10:59 -0400

    tweak

--- gslp/VLoop.cpp
+++ gslp/VLoop.cpp
@@ -2,31 +2,134 @@
 #include "DependenceAnalysis.h"
 #include "VectorPackContext.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/ScalarEvolutionExpressions.h"
 
 using namespace llvm;
 
-VLoop::VLoop(LoopInfo &LI, Loop *L, VectorPackContext *VPCtx, GlobalDependenceAnalysis *DA) : Depended(VPCtx->getNumValues()) {
+static void setSubtract(BitVector &Src, BitVector ToRemove) {
+  ToRemove.flip();
+  Src &= ToRemove;
+}
+
+VLoop::VLoop(LoopInfo &LI, Loop *L, VectorPackContext *VPCtx,
+             GlobalDependenceAnalysis *DA)
+    : Depended(VPCtx->getNumValues()), Insts(VPCtx->getNumValues()), L(L),
+      Parent(nullptr) {
   assert(L->isRotatedForm());
-  // Build the subloops first
+
+  auto *LoopBr = cast<BranchInst>(L->getLoopLatch()->getTerminator());
+  LoopCond = LoopBr->getCondition();
+  assert(LoopCond);
+  LoopIfTrue = LoopBr->getSuccessor(0) == L->getHeader();
+
+  // Build the sub-loops first
   for (auto *SubL : *L) {
     auto *SubVL = new VLoop(LI, SubL, VPCtx, DA);
-    LoopStorage.emplace_back(SubVL);
-    SubLoops.push_back(SubVL);
+    SubLoops.emplace_back(SubVL);
+    SubVL->Parent = this;
     Depended |= SubVL->getDepended();
+    Insts |= SubVL->Insts;
   }
 
-  llvm::BitVector InstSet(VPCtx->getNumValues());
   for (auto *BB : L->blocks()) {
     if (LI.getLoopFor(BB) != L)
       continue;
     for (auto &I : *BB) {
       Depended |= DA->getDepended(&I);
-      Insts.push_back(&I);
-      InstSet.set(VPCtx->getScalarId(&I));
+      TopLevelInsts.push_back(&I);
+      Insts.set(VPCtx->getScalarId(&I));
     }
   }
 
-  // Remove dependences that are in this loop
-  InstSet.flip();
-  Depended &= InstSet;
+  setSubtract(Depended, Insts);
+}
+
+static bool haveIdenticalTripCounts(const Loop *L1, const Loop *L2,
+                                    ScalarEvolution &SE) {
+  if (L1 == L2)
+    return true;
+
+  auto *TripCount1 = SE.getBackedgeTakenCount(L1);
+  auto *TripCount2 = SE.getBackedgeTakenCount(L2);
+  if (!isa<SCEVCouldNotCompute>(TripCount1) &&
+      !isa<SCEVCouldNotCompute>(TripCount2) && TripCount1 == TripCount2)
+    return true;
+
+  auto *BB1 = L1->getExitingBlock();
+  if (!BB1)
+    return false;
+
+  auto *BB2 = L2->getExitingBlock();
+  if (!BB2)
+    return false;
+
+  using namespace PatternMatch;
+
+  Value *LHS1, *RHS1;
+  ICmpInst::Predicate Pred1;
+  if (!match(BB1->getTerminator(),
+             m_Br(m_ICmp(Pred1, m_Value(LHS1), m_Value(RHS1)), m_BasicBlock(),
+                  m_BasicBlock())))
+    return false;
+
+  Value *LHS2, *RHS2;
+  ICmpInst::Predicate Pred2;
+  if (!match(BB2->getTerminator(),
+             m_Br(m_ICmp(Pred2, m_Value(LHS2), m_Value(RHS2)), m_BasicBlock(),
+                  m_BasicBlock())))
+    return false;
+
+  if (Pred1 != Pred2)
+    return false;
+
+  if (RHS1 != RHS2)
+    return false;
+
+  auto *Expr1 = dyn_cast<SCEVAddRecExpr>(SE.getSCEV(LHS1));
+  if (!Expr1 || !Expr1->isAffine() || Expr1->getLoop() != L1)
+    return false;
+
+  auto *Expr2 = dyn_cast<SCEVAddRecExpr>(SE.getSCEV(LHS2));
+  if (!Expr2 || !Expr1->isAffine() || Expr2->getLoop() != L2)
+    return false;
+
+  return Expr1->getOperand(0) == Expr2->getOperand(0) &&
+         Expr1->getOperand(1) == Expr2->getOperand(1);
+}
+
+bool VLoop::isSafeToFuse(const VLoop *VL1, const VLoop *VL2,
+                         ScalarEvolution &SE) {
+  if (VL1 == VL2)
+    return true;
+
+  // Loop level mismatch
+  if (!VL1 || !VL2)
+    return false;
+
+  if (!haveIdenticalTripCounts(VL1->L, VL2->L, SE))
+    return false;
+
+  // Make sure the loops are independent
+  if (VL1->Insts.anyCommon(VL2->Depended) ||
+      VL2->Insts.anyCommon(VL1->Depended))
+    return false;
+
+  return isSafeToFuse(VL1->Parent, VL2->Parent, SE);
+}
+
+void VLoop::fuse(VLoop *VL1, VLoop *VL2) {
+  auto *Parent = VL1->Parent;
+  if (Parent != VL2->Parent) {
+    assert(Parent && VL2->Parent);
+    fuse(VL1->Parent, VL2->Parent);
+  }
+  VL1->Insts |= VL2->Insts;
+  VL1->Depended |= VL2->Depended;
+  VL1->TopLevelInsts.append(VL2->TopLevelInsts);
+  auto It = llvm::find_if(Parent->SubLoops, [VL2](std::unique_ptr<VLoop> &VL) {
+    return VL.get() == VL2;
+  });
+  assert(It != Parent->SubLoops.end());
+  Parent->SubLoops.erase(It);
 }
--- gslp/VLoop.h
+++ gslp/VLoop.h
@@ -9,6 +9,7 @@ class Loop;
 class Instruction;
 class Value;
 class LoopInfo;
+class ScalarEvolution;
 } // namespace llvm
 
 class VectorPackContext;
@@ -17,21 +18,25 @@ class GlobalDependenceAnalysis;
 class VLoop {
   // Instructions that this loop dependes on (and not in the loop)
   llvm::BitVector Depended;
-  std::vector<llvm::Instruction *> Insts;
-  llvm::SmallVector<VLoop *, 4> SubLoops;
+  // All instructions included in the loop (including those in the subloops)
+  llvm::BitVector Insts;
+  llvm::SmallVector<llvm::Instruction *> TopLevelInsts;
+  llvm::SmallVector<std::unique_ptr<VLoop>, 4> SubLoops;
 
   llvm::Value *LoopCond;
   bool LoopIfTrue; // indicate how LoopCond is used
 
   VLoop *Parent;
-
-  llvm::SmallVector<std::unique_ptr<VLoop>, 4> LoopStorage;
+  llvm::Loop *L; // the original loop
 
 public:
   VLoop(llvm::LoopInfo &, llvm::Loop *L, VectorPackContext *VPCtx, GlobalDependenceAnalysis *DA);
-  llvm::ArrayRef<llvm::Instruction *> getInstructions() const { return Insts; }
-  llvm::ArrayRef<VLoop *> getSubLoops() const { return SubLoops; }
+  llvm::ArrayRef<llvm::Instruction *> getInstructions() const { return TopLevelInsts; }
+  llvm::ArrayRef<std::unique_ptr<VLoop>> getSubLoops() const { return SubLoops; }
   const llvm::BitVector &getDepended() const { return Depended; }
+
+  static bool isSafeToFuse(const VLoop *, const VLoop *, llvm::ScalarEvolution &);
+  static void fuse(VLoop *, VLoop *);
 };
 
 #endif
