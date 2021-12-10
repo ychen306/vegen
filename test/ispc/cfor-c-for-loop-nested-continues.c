6a8877e2722bccc2fb150c08e253e996e8971acb
tom <ychen306@mit.edu>
2021-12-10 12:17:19 -0500

    more nested tests

---
+++ test/ispc/cfor-c-for-loop-nested-continues.c
@@ -0,0 +1,34 @@
+// RUN: %clang-o3 -march=native -mllvm -filter=kernel %s -o %t && %t
+
+__attribute__((noinline))
+void kernel(int n, float *RET, float *restrict aFOO) {
+  for (int programIndex = 0; programIndex < n; programIndex++) {
+    float a = aFOO[programIndex&0x3];
+    int i, j;
+    float r = 0;
+    for (i = 0; i < a+1; ++i) {
+      if (i == 1)
+        continue;
+      for (j = 0; j < a; ++j) {
+        if (a == 2)
+          continue;
+      }
+      ++r;
+    }
+    RET[programIndex] = r;
+  }
+}
+
+int main() {
+  int n = 1030;
+  float a[n], ret[n];
+  for (int i = 0; i < n; i++)
+    a[i] = i+1;
+
+  kernel(n, ret, a);
+
+  for (int i = 0; i < n; i++)
+    if (ret[i] != 1 + i%4)
+      return 1;
+  return 0;
+}
